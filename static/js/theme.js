9f80a6616015b258eb1cd83afaabd43db89cc7ba
jeakey <june_fh@qq.com>
2014-09-23 13:04:51 +0800

    add ta

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,20 +1,18 @@
 (function($) {
 
- 
   $('.nico-iframe iframe').on('mouseover',function(){
-   
     document.addEventListener('mousewheel',preventEvent,false); 
   });
+
   $('.nico-iframe iframe').on('mouseout',function(){
     document.removeEventListener('mousewheel',preventEvent,false); 
   });
 
   // 取消默认事件
-  function preventEvent(evt){
-
+  function preventEvent(evt) {
     evt.preventDefault();
-        
   }
+
 })($);
 
 
